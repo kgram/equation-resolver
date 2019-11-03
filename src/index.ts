617538b95accb1c3087884f601ad0e00b707fbd7
Kristoffer Gram <kristoffer@subhub.dk>
2019-11-03 17:30:21 +0100

    Export relevant types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,12 @@ export { resolve } from './resolve'
 export { format } from './format'
 export { defaultFunctions } from './defaultFunctions'
 export { defaultVariables } from './defaultVariables'
+
+import * as ResultNode from './ResultNode'
+export type ResultNode = ResultNode.ResultNode
+export type ResultNodeNumber = ResultNode.ResultNodeNumber
+export type ResultNodeMatrix = ResultNode.ResultNodeMatrix
+export type ResultNodeUnit = ResultNode.ResultNodeUnit
+
+import * as UnitLookup from './UnitLookup'
+export type UnitLookup = UnitLookup.UnitLookup
