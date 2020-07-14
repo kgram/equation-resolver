bbfa4d8d82a93acbba57214dcf303504e974bb6c
Kristoffer Gram <kristoffer@subaio.com>
2020-07-14 16:31:32 +0200

    Fix barrel exports of types

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,33 @@ export { format } from './format'
 export { defaultFunctions } from './defaultFunctions'
 export { defaultVariables } from './defaultVariables'
 
-import * as ResultNode from './ResultNode'
-export type ResultNode = ResultNode.ResultNode
-export type ResultNodeNumber = ResultNode.ResultNodeNumber
-export type ResultNodeMatrix = ResultNode.ResultNodeMatrix
-export type ResultNodeUnit = ResultNode.ResultNodeUnit
+export type {
+    ResultNode,
+    ResultNodeNumber,
+    ResultNodeMatrix,
+    ResultNodeUnit,
+} from './ResultNode'
 
-import * as UnitLookup from './UnitLookup'
-export type UnitLookup = UnitLookup.UnitLookup
+export type {
+    UnitLookup,
+} from './UnitLookup'
 
-import * as ResultResolveError from './ResultResolveError'
-export type ResultResolveError = ResultResolveError.ResultResolveError
+export type {
+    ResultResolveError,
+} from './ResultResolveError'
 
-import * as EquationResolveError from './EquationResolveError'
-export type EquationResolveError = EquationResolveError.EquationResolveError
+export type {
+    EquationResolveError,
+} from './EquationResolveError'
 
-import * as FunctionLookup from './FunctionLookup'
-export type FunctionLookup = FunctionLookup.FunctionLookup
+export type {
+    FunctionLookup,
+} from './FunctionLookup'
 
-import * as VariableLookup from './VariableLookup'
-export type VariableLookup = VariableLookup.VariableLookup
+export type {
+    VariableLookup,
+} from './VariableLookup'
 
-import * as FormatOptions from './FormatOptions'
-export type FormatOptions = FormatOptions.FormatOptions
+export type {
+    FormatOptions,
+} from './FormatOptions'
